d4e259e31f7ef81aad59ee0af2e6904ae7d38cfa
Robert Scott <code@humanleg.org.uk>
2017-07-16 12:18:04 +0100

    QuadTreeNodeTest: parametrize & expand

--- test/unit/org/openstreetmap/josm/plugins/markseen/QuadTreeNodeTest.java
+++ test/unit/org/openstreetmap/josm/plugins/markseen/QuadTreeNodeTest.java
@@ -1,7 +1,11 @@
 package org.openstreetmap.josm.plugins.markseen;
 
 import java.io.IOException;
+import java.lang.Boolean;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
@@ -14,15 +18,22 @@ import org.openstreetmap.josm.data.Bounds;
 import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
+import org.junit.Rule;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 
-@RunWith(MockitoJUnitRunner.class)
-public class QuadTreeNodeTestSingleRect extends BaseTest {
+@RunWith(Parameterized.class)
+public class QuadTreeNodeTest extends BaseTest {
+    @Rule public MockitoRule rule = MockitoJUnit.rule();
+
     @Mock private MarkSeenTileController tileController;
     @Mock private TileSource tileSource;
 
@@ -32,6 +43,38 @@ public class QuadTreeNodeTestSingleRect extends BaseTest {
     private int tileSize = 256;
 
     private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
+    private Bounds bounds;
+    private double minTilesWidth;
+    private Object[][] referenceTiles;
+
+    @Parameters
+    public static Collection<Object[]> getParameters() throws IOException {
+        return Arrays.asList(new Object[][] {
+            {
+                new Bounds(51.36, -0.35, 51.61, 0.10),
+                4,
+                new Object[][] {
+                    // zoom, xtile, ytile, expectedMask
+                    // the expectedMask can either be a byte[] OR a boolean, true denoting FULL_MASK and false
+                    // EMPTY_MASK (we can't reference these directly because they haven't been allocated at this point)
+                    { 10, 50, 50, false },
+                    { 15, 16365, 10867, false },
+                    { 14, 8184, 5448, true },
+                    { 11, 1023, 681, true },
+                    { 16, 32730, 21762, byteArrayFromResource("QuadTreeNodeTest/testSingleRect/0/16-32730-21762.bin") },
+                    { 11, 1024, 680, byteArrayFromResource("QuadTreeNodeTest/testSingleRect/0/11-1024-680.bin") },
+                    { 7, 63, 42, byteArrayFromResource("QuadTreeNodeTest/testSingleRect/0/7-63-42.bin") }
+                }
+            }
+        });
+    }
+
+    public QuadTreeNodeTest(Bounds bounds_, double minTilesWidth_, Object[][] referenceTiles_) {
+        this.bounds = bounds_;
+        this.minTilesWidth = minTilesWidth_;
+        this.referenceTiles = referenceTiles_;
+    }
 
     @Before
     public void setUp() {
@@ -68,65 +111,46 @@ public class QuadTreeNodeTestSingleRect extends BaseTest {
     }
 
     @Test
-    public void test() throws IOException {
+    public void test() {
         QuadTreeNode quadTreeRoot = new QuadTreeNode(this.tileController);
         this.lock.writeLock().lock();
-        quadTreeRoot.markRectSeen(new Bounds(51.36, -0.35, 51.61, 0.10), 4, this.tileController);
+        quadTreeRoot.markRectSeen(this.bounds, this.minTilesWidth, this.tileController);
         quadTreeRoot.checkIntegrity();
-        BufferedImage result;
-        byte[] loadedByteArray;
 
-        result = quadTreeRoot.getNodeForTile(
-            50,
-            50,
-            13,
-            true,
-            this.tileController
-        ).getMask(true, this.tileController);
-        quadTreeRoot.checkIntegrity();
-        assertArrayEquals(
-            ((DataBufferByte) result.getData().getDataBuffer()).getData(),
-            ((DataBufferByte) this.EMPTY_MASK.getData().getDataBuffer()).getData()
-        );
+        for (int i = 0; i<referenceTiles.length; i++) {
+            Object[] referenceTileInfo = this.referenceTiles[i];
+            System.out.format("Checking reference tile %d\n", i);
+            int zoom = (int)referenceTileInfo[0];
+            int tilex = (int)referenceTileInfo[1];
+            int tiley = (int)referenceTileInfo[2];
+            byte[] maskBytes = Boolean.class.isInstance(referenceTileInfo[3]) ?
+                (
+                    (DataBufferByte) (
+                        ((boolean)referenceTileInfo[3]) ?
+                                      this.FULL_MASK :
+                                      this.EMPTY_MASK
+                    ).getData().getDataBuffer()
+                ).getData() :
+                (byte[])referenceTileInfo[3];
 
-        result = quadTreeRoot.getNodeForTile(
-            8184,
-            5448,
-            14,
-            true,
-            this.tileController
-        ).getMask(true, this.tileController);
-        quadTreeRoot.checkIntegrity();
-        assertArrayEquals(
-            ((DataBufferByte) result.getData().getDataBuffer()).getData(),
-            ((DataBufferByte) this.FULL_MASK.getData().getDataBuffer()).getData()
-        );
+            BufferedImage result = quadTreeRoot.getNodeForTile(
+                tilex,
+                tiley,
+                zoom,
+                true,
+                this.tileController
+            ).getMask(true, this.tileController);
+            quadTreeRoot.checkIntegrity();
 
-        result = quadTreeRoot.getNodeForTile(
-            1023,
-            681,
-            11,
-            true,
-            this.tileController
-        ).getMask(true, this.tileController);
-        quadTreeRoot.checkIntegrity();
-        assertArrayEquals(
-            ((DataBufferByte) result.getData().getDataBuffer()).getData(),
-            ((DataBufferByte) this.FULL_MASK.getData().getDataBuffer()).getData()
-        );
+//             System.out.println(javax.xml.bind.DatatypeConverter.printHexBinary(
+//                 ((DataBufferByte) result.getData().getDataBuffer()).getData())
+//             );
 
-        result = quadTreeRoot.getNodeForTile(
-            1024,
-            680,
-            11,
-            true,
-            this.tileController
-        ).getMask(true, this.tileController);
-        quadTreeRoot.checkIntegrity();
-        assertArrayEquals(
-            ((DataBufferByte) result.getData().getDataBuffer()).getData(),
-            byteArrayFromResource("QuadTreeNodeTest/testSingleRect/0/11-1024-680.bin")
-        );
+            assertArrayEquals(
+                ((DataBufferByte) result.getData().getDataBuffer()).getData(),
+                maskBytes
+            );
+        }
 
         this.lock.writeLock().unlock();
     }
