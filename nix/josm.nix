c9caac01ed021e5c872688e34a678bdf6044a79a
Robert Scott <code@humanleg.org.uk>
2017-11-04 14:16:49 +0000

    josm.nix: improve ability to work with historic josm versions

--- nix/josm.nix
+++ nix/josm.nix
@@ -2,25 +2,28 @@
 
 antBuild rec {
   name = "josm-${version}";
-  version = "12545";
+  version = "13053";
 
   src = fetchsvn rec {
     name = "josm-r${rev}";
     url = "https://josm.openstreetmap.de/svn/trunk";
-    sha256 = "1f9w8dsmh79gpzdglmh4vnpkmdk1nx4f4k80qvsz4sbf7b8yy8sd";
+    sha256 = builtins.getAttr rev {
+      "12545" = "1f9w8dsmh79gpzdglmh4vnpkmdk1nx4f4k80qvsz4sbf7b8yy8sd";  # 2017-08-13 release
+      "13053" = "12b1ai0gb9akm2jb95dlnmasgm20zlq4782y0skpsp05bgr02lf1";  # 2017-10-29 release
+    };
     rev=version;
   };
 
-  patches = [
-    (
-      if
-        (builtins.compareVersions version "12620") == -1
-      then
-        ./josm-build-without-javafx-pre-r12620.patch
-      else
-        ./josm-build-without-javafx-r12620-onwards.patch
-    )
-  ];
+  patches = [] ++ stdenv.lib.optional (
+    (builtins.compareVersions version "12715") == -1 && (builtins.compareVersions version "12328") != -1
+  ) (
+    if
+      (builtins.compareVersions version "12620") == -1
+    then
+      ./josm-build-without-javafx-pre-r12620.patch
+    else
+      ./josm-build-without-javafx-r12620-onwards.patch
+  );
 
   antTargets = [
     "dist"
